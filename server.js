bf5dc3da94b38f1dd8db25bd87ad67f5dd912a11
vishalkumarkashyap <vishalku.1995@gmail.com>
2017-03-01 17:12:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile('this is article one');
-});
+
 
 app.get('/article-two', function (req, res) {
   res.sendFile('this is article two');
