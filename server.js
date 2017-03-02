16b7008eab4df32b26c743f7aa54ec0810adf50f
vishalkumarkashyap <vishalku.1995@gmail.com>
2017-03-02 19:49:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/article-one',function(req,res){
+   res.sendFile('artile one is requsted and will be served here'); 
+});
+
+app.get('/article-two',function(req,res){
+   res.sendFile('artile two is requsted and will be served here'); 
+});
+
+app.get('/article-three',function(req,res){
+   res.sendFile('artile three is requsted and will be served here'); 
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
